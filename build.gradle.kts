0c05d336994898992023c94d3e4b02457a1c9f7f
Alexander Brandes <mc.cache@web.de>
2023-10-05 19:31:54 +0200

    Back to snapshot for development

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
-version = "1.4"
+version = "1.5-SNAPSHOT"
 group = "com.intellectualsites.informative-annotations"
 
 repositories {
